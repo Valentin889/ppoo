86284c8b427e5160492e97dfe9f908797e76ecf6
Valentin889 <thebiggbosse889@gmail.com>
2022-03-02 13:41:56 +0100

    Add function byteFrequencies

--- Serie2/src/cs108/Main.java
+++ Serie2/src/cs108/Main.java
@@ -1,8 +1,32 @@
 package cs108;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
 
-        System.out.println("Salut");
+        try {
+            int[] b = byteFrequencies("./STAT/file0.bin");
+            System.out.println(Arrays.toString(b));
+        } catch(IOException e){
+            System.out.println("Error");
+        }
+    }
+
+    private static int[] byteFrequencies(String fileName) throws IOException {
+
+        InputStream stream = new FileInputStream(fileName);
+
+        int[] valReturn = new int[256];
+
+        int b = 0;
+        while((b = stream.read()) != -1){
+            valReturn[b]++ ;
+        }
+
+        stream.close();
+
+        return valReturn;
     }
 }
