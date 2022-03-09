e35b46c3e999acffd0a4915189f373e6c9247cf1
Valentin889 <thebiggbosse889@gmail.com>
2022-03-09 14:29:56 +0100

    End exo1

--- serie3/src/cs108/Steganographer.java
+++ serie3/src/cs108/Steganographer.java
@@ -7,6 +7,34 @@ public final class Steganographer {
 
     public static String extract(BufferedImage image) {
         // TODO: à faire (exercice 1)
+        String binarytext = "";
+        int t = 0;
+        int t2 = 0;
+        int count = 0;
+
+
+        for (int i = 0; i < image.getTileHeight(); i++) {
+            for (int j = 0; j < image.getTileWidth(); j++) {
+
+                int val = image.getRGB(j,i) & 1;
+                t += val << (15-count);
+                count++;
+
+               if(count == 16){
+                    binarytext += (char)t;
+                    count = 0;
+                    t=0;
+                }
+            }
+        }
+
+
+        System.out.println(binarytext);
+
+
+
+
+
         return null;
     }
 
