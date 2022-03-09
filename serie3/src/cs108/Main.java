5d3522adbf451d6bd99ea72091b23ea9f9f60303
Valentin889 <thebiggbosse889@gmail.com>
2022-03-09 13:03:43 +0100

    Add template for start

---
+++ serie3/src/cs108/Main.java
@@ -0,0 +1,15 @@
+package cs108;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public final class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedImage image = ImageIO.read(new File("pskov.png"));
+        String text = Steganographer.extract(image);
+        System.out.println("Texte extrait de l'image :");
+        System.out.println(text);
+    }
+}
---
+++ serie3/src/cs108/Steganographer.java
@@ -0,0 +1,17 @@
+package cs108;
+
+import java.awt.image.BufferedImage;
+
+public final class Steganographer {
+    private Steganographer() { }
+
+    public static String extract(BufferedImage image) {
+        // TODO: à faire (exercice 1)
+        return null;
+    }
+
+    public static BufferedImage insert(BufferedImage image, String string) {
+        // TODO: à faire (exercice 2)
+        return null;
+    }
+}
