325920c932f90d1f719c2296ea348d24a3eeee68
Valentin889 <thebiggbosse889@gmail.com>
2022-03-09 15:01:28 +0100

    Try to finish exo2

--- serie3/src/cs108/Steganographer.java
+++ serie3/src/cs108/Steganographer.java
@@ -9,12 +9,11 @@ public final class Steganographer {
         // TODO: à faire (exercice 1)
         String binarytext = "";
         int t = 0;
-        int t2 = 0;
         int count = 0;
 
 
-        for (int i = 0; i < image.getTileHeight(); i++) {
-            for (int j = 0; j < image.getTileWidth(); j++) {
+        for (int i = 0; i < image.getHeight(); i++) {
+            for (int j = 0; j < image.getWidth(); j++) {
 
                 int val = image.getRGB(j,i) & 1;
                 t += val << (15-count);
@@ -29,17 +28,52 @@ public final class Steganographer {
         }
 
 
-        System.out.println(binarytext);
 
 
 
-
-
-        return null;
+        return binarytext;
     }
 
     public static BufferedImage insert(BufferedImage image, String string) {
-        // TODO: à faire (exercice 2)
-        return null;
+
+        int t = 0;
+        int count1 = 0;
+        int count2 = 0;
+        char c = string.charAt(0);
+
+
+
+
+        for (int i = 0; i < image.getHeight(); i++) {
+            for (int j = 0; j < image.getWidth(); j++) {
+                //int val = image.getRGB(j,i) & 1;
+
+                c = 0b1111000000000000;
+
+                int val = (int)c << 16 + count1;
+                System.out.println(val);
+                val = val >>> 15;
+                System.out.println(val);
+                image.setRGB(j,i,image.getRGB(j,i) | val);
+
+
+                count1++;
+
+                if(count1 == 16){
+                    count2++;
+                    c = 0;
+                    if(string.length() > count2){
+                        c = string.charAt(count2);
+                    } else {
+                        i = image.getHeight();
+                        j = image.getWidth();
+                    }
+                }
+            }
+        }
+
+
+
+        return image;
     }
 }
