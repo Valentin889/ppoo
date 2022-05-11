3b7ffa00531e0a746c8c61b1384ee35385909672
Valentin889 <thebiggbosse889@gmail.com>
2022-05-11 14:50:41 +0200

    End exo2

--- serie10/src/cs108/Main.java
+++ serie10/src/cs108/Main.java
@@ -18,10 +18,35 @@ public final class Main extends Application {
         ImageView imageView = new ImageView();
         imageView.imageProperty().bind(mandelbrot.imageProperty());
 
+
+
+
+
+        imageView.setOnMouseClicked(e -> {
+            if(e.getClickCount() == 2){
+                Rectangle rectangle = mandelbrot.getFrame();
+
+                double height = mandelbrot.height();
+                double ny = height - e.getY();
+
+                double yConverter = rectangle.height() / mandelbrot.height();
+                double xConverter = rectangle.width() / mandelbrot.width();
+
+                double nxMandelbrot =  e.getX() * xConverter;
+                double nyMandelbrot = ny * yConverter;
+
+                rectangle = rectangle.translatedBy(nxMandelbrot, nyMandelbrot);
+                rectangle = rectangle.scaledBy(1/2.0, 1/2.0);
+                rectangle =  rectangle.translatedBy(-nxMandelbrot / 2, -nyMandelbrot/2);
+
+                mandelbrot.setFrameCenter(rectangle.center());
+                mandelbrot.setFrameWidth(rectangle.width());
+            }
+        });
+
         BorderPane mainPane = new BorderPane(imageView);
         mandelbrot.widthProperty().bind(mainPane.widthProperty());
         mandelbrot.heightProperty().bind(mainPane.heightProperty());
-
 
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.setTitle("Mandelbrot");
